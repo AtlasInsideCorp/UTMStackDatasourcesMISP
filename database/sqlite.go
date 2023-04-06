28c42fb93ca821e09a2602139230ca9fbde309df
Yorjander Hernández Vergara <yorjander@atlasinside.com>
2023-04-06 05:01:51 -0400

    Adding dataBase type and add Migrate function

--- database/sqlite.go
+++ database/sqlite.go
@@ -1,1 +1,20 @@
 package database
+
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+type dataBase struct {
+	connection *gorm.DB
+}
+
+// Migrate create a new table in the dataBase
+func (s *dataBase) Migrate(data interface{}) error {
+	err := s.connection.AutoMigrate(&data)
+	if err != nil {
+		return fmt.Errorf("failed to create new table in database: %v", err)
+	}
+	return nil
+}
